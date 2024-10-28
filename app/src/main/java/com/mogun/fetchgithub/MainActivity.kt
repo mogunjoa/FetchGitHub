5c683fc6bf2516160c0f3fd94e026bb4efd34444
seungohjin <corrobolea@gmail.com>
2024-10-28 17:27:45 +0900

    update: 유저불러오기 검색화면 구현

--- app/src/main/java/com/mogun/fetchgithub/MainActivity.kt
+++ app/src/main/java/com/mogun/fetchgithub/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.mogun.fetchgithub
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mogun.fetchgithub.adapter.UserAdapter
 import com.mogun.fetchgithub.databinding.ActivityMainBinding
-import com.mogun.fetchgithub.model.Repo
 import com.mogun.fetchgithub.model.UserDto
 import com.mogun.fetchgithub.network.GithubService
 import retrofit2.Call
@@ -16,41 +22,66 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var userAdapter: UserAdapter
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var searchFor: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        binding.searchRepo.setOnClickListener {
+//            githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
+//                override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+//                    Log.e("[RES:::]", "SEARCH_REPO:::${response.body().toString()}")
+//                }
+//
+//                override fun onFailure(call: Call<List<Repo>>, response: Throwable) {
+//                    Log.e("[Fail:::]", response.toString())
+//                }
+//            })
+//        }
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        userAdapter = UserAdapter(onClick = {
+            val intent = Intent(this, RepoActivity::class.java)
+            intent.putExtra("username", it.username)
+            startActivity(intent)
+        })
 
+        binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userAdapter
+        }
+
+        val runnable = Runnable {
+            searchUser()
+        }
+
+        binding.searchEditText.addTextChangedListener {
+            searchFor = it.toString()
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, 300)
+        }
+    }
+
+    private fun searchUser() {
         val githubService = retrofit.create(GithubService::class.java)
 
-        binding.searchRepo.setOnClickListener {
-            githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
-                override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                    Log.e("[RES:::]", "SEARCH_REPO:::${response.body().toString()}")
-                }
+        githubService.searchUsers(searchFor).enqueue(object : Callback<UserDto> {
+            override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
+                Log.e("[RES:::]", "SEARCH_USER:::${response.body().toString()}")
+                userAdapter.submitList(response.body()?.items)
+            }
 
-                override fun onFailure(call: Call<List<Repo>>, response: Throwable) {
-                    Log.e("[Fail:::]", response.toString())
-                }
-            })
-        }
-
-        binding.searchUser.setOnClickListener {
-            githubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
-                override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
-                    Log.e("[RES:::]", "SEARCH_USER:::${response.body().toString()}")
-                }
-
-                override fun onFailure(call: Call<UserDto>, response: Throwable) {
-                    Log.e("[Fail:::]", response.toString())
-                }
-            })
-        }
+            override fun onFailure(call: Call<UserDto>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "에러가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                t.printStackTrace()
+            }
+        })
     }
 }
